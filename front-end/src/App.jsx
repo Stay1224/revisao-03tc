1fc94d593294f6a7ba1efbfdbd132c39a73d7409
Kelt <kelton.tinho23@gmail.com>
2023-08-14 15:13:01 -0300

    estado do usuário

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,27 +2,35 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [usuario, setUsuario] = useState("")
  
+  function handleUsuario(evento) {
+  setUsuario(evento.target.value)
+ 
+  }
 
+ console.log(usuario)
+ 
   return (
+
     <>
     <header>
-      <h1>Login</h1>
-    </header>
+        <h1>Login</h1>
+              </header>
 
-    <main>
-      <form action="">
-        <div>
-          <label htmlFor="usuario">Usuário</label>
-        <input type="text" id= "usuario" placeholder='Digite seu usuário' />
-        </div>
-        <div>
-        <label htmlFor="senha">Senha</label>
-        <input type="password" id= "senha" placeholder='Digite sua senha' />
-          </div>  
+      <main>
+        <form action="">
+          <div>
+            <label htmlFor="usuario">Usuário</label>
+            <input type="text" id="usuario" placeholder='Digite seu usuário' onChange={handleUsuario}/>
+          </div>
+          <div>
+            <label htmlFor="senha">Senha</label>
+            <input type="password" id="senha" placeholder='Digite sua senha' />
+          </div>
           <button>Entrar</button>
-      </form>
-    </main>
+        </form>
+      </main>
     </>
   )
 }
